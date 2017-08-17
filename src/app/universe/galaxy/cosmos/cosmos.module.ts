d307f1fba3e0982df8e0960d0c391dd22620c972
Nattha <nattha@natthas-macbook-pro.home>
2017-08-18 01:49:39 +0200

    did stuff

--- src/app/universe/galaxy/cosmos/cosmos.module.ts
+++ src/app/universe/galaxy/cosmos/cosmos.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CosmosComponent } from './cosmos/cosmos.component';
+import {NgModule} from '@angular/core';
+import {CosmosComponent} from './cosmos/cosmos.component';
 import {RouterModule, Routes} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {OverviewComponent} from './overview/overview.component';
 
 export const cosmosRoutes: Routes = [
   {
     path: 'cosmos',
     component: CosmosComponent,
+    children: [
+      {path: '', redirectTo: 'overview', pathMatch: 'full'},
+      {path: 'overview', component: OverviewComponent}
+    ]
   },
 ];
 
@@ -21,7 +26,9 @@ export const cosmosRoutes: Routes = [
     HttpModule,
   ],
   declarations: [
-    CosmosComponent
+    CosmosComponent,
+    OverviewComponent
   ],
 })
-export class CosmosModule { }
+export class CosmosModule {
+}
