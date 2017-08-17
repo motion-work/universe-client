cca1ac91aa6fd82b5f5e4baa7a028f79155035dd
Nattha <nattha@Natthas-MacBook-Pro.local>
2017-08-17 11:41:37 +0200

    updated oauth credentials for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   grant_type: 'password',
-  client_id: 3,
-  client_secret: 'S2RQuWXC380G8EgTh3k9OEfpItdFz2wP4GRd8mY0',
+  client_id: 5,
+  client_secret: 'vpX3DxujGKSLTZZbtNv88RHnTH0pARoeU7wjC1Wx',
   api_host: 'https://api.universe.rocketstart.ch/',
 };
