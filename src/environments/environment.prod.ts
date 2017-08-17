c62defd4bed81b92fb96ddd3f1886e114dad7fda
Nattha <nattha@Natthas-MacBook-Pro.local>
2017-08-17 11:01:35 +0200

    https bitches

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   grant_type: 'password',
   client_id: 3,
   client_secret: 'S2RQuWXC380G8EgTh3k9OEfpItdFz2wP4GRd8mY0',
-  api_host: 'https://universe.rocketstart.ch/',
+  api_host: 'https://api.universe.rocketstart.ch/',
 };
